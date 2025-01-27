fc3b84da9b3fc4da887a9db95d6b63517e2a3c00
Nar Mena <narciris.tech@gmail.com>
2025-01-27 00:48:35 -0500

    feat:creating goals methods

---
+++ src/main/java/com/BI/dto/ResponseDto/GoalResponseDto.java
@@ -0,0 +1,72 @@
+package com.BI.dto.ResponseDto;
+
+import com.BI.Utils.Status;
+
+import java.util.List;
+
+public class GoalResponseDto {
+
+    private String userId;
+    private String title;
+    private Double currentAmount;
+    private Double goalAmount;
+    private Status status;
+    private List<SuggestionsDto> suggetionsUSer;
+public GoalResponseDto() {}
+    public GoalResponseDto(String userId, String title, Double currentAmount, Double goalAmount, Status status, List<SuggestionsDto> suggetionsUSer) {
+        this.userId = userId;
+        this.title = title;
+        this.currentAmount = currentAmount;
+        this.goalAmount = goalAmount;
+        this.status = status;
+        this.suggetionsUSer = suggetionsUSer;
+    }
+
+    public List<SuggestionsDto> getSuggetionsUSer() {
+        return suggetionsUSer;
+    }
+
+    public void setSuggetionsUSer(List<SuggestionsDto> suggetionsUSer) {
+        this.suggetionsUSer = suggetionsUSer;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Double getCurrentAmount() {
+        return currentAmount;
+    }
+
+    public void setCurrentAmount(Double currentAmount) {
+        this.currentAmount = currentAmount;
+    }
+
+    public Double getGoalAmount() {
+        return goalAmount;
+    }
+
+    public void setGoalAmount(Double goalAmount) {
+        this.goalAmount = goalAmount;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+}
---
+++ src/main/java/com/BI/dto/ResponseDto/SuggestionsDto.java
@@ -0,0 +1,17 @@
+package com.BI.dto.ResponseDto;
+
+public class SuggestionsDto {
+    private String suggestion;
+    public SuggestionsDto(String suggestion) {}
+
+    public String getSuggestion() {
+        return suggestion;
+    }
+
+    public void setSuggestion(String suggestion) {
+        this.suggestion = suggestion;
+    }
+
+
+
+}
