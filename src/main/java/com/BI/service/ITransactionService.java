9fd83cadd56c5750852a984e53941d2237a329e7
SoyNar <155842504+SoyNar@users.noreply.github.com>
2025-01-27 21:50:06 -0500

    feat:implementing prospective methods (#6)

--- src/main/java/com/BI/service/ITransactionService.java
+++ src/main/java/com/BI/service/ITransactionService.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 public interface ITransactionService {
 
+    List<Transactions> getTransactionByUserAndMonth(int userId, String month);
     List<Transactions> getTransactions(int userId);
 
 }
