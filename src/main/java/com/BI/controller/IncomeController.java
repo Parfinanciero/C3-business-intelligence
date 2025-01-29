caa47265d8cf429cccee2c0ad9970987f106fad0
SoyNar <narciris.tech@gmail.com>
2025-01-28 19:53:52 -0500

    feat: update swagger documentation

--- src/main/java/com/BI/controller/IncomeController.java
+++ src/main/java/com/BI/controller/IncomeController.java
@@ -4,6 +4,8 @@ import com.BI.dto.ResponseDto.CashResponseDto;
 import com.BI.dto.ResponseDto.GetTransactionResponse;
 import com.BI.service.IncomeService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,12 +31,34 @@ public class IncomeController {
 
 //    metodo para obtener los gastos totales de un usuario por su id
 
-    @Operation( summary = "Obtner Ingresos de un usuario",
-            description = "Aqui podras obtener los ingresos de un usuario por mes")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "201", description = "Expenses successfully retrieved"),
-            @ApiResponse(responseCode = "500", description = "Internal server error")
-    }
+    @Operation(
+            summary = "Obtiene el total de ingresos de un usuario por mes (Faker)",
+            description = "Este método simula una llamada a una API externa y genera aleatoriamente los ingresos " +
+                    "de un usuario para un mes específico, usando Java Faker. Devuelve la suma total de ingresos.",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Cálculo exitoso de la suma de ingresos para el usuario y mes especificados",
+                            content = @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = CashResponseDto.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "Solicitud incorrecta, posiblemente debido a un formato de entrada no válido",
+                            content = @Content(mediaType = "application/json")
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Usuario no encontrado o ingresos no disponibles para el mes solicitado",
+                            content = @Content(mediaType = "application/json")
+                    ),
+                    @ApiResponse(
+                            responseCode = "500",
+                            description = "Error interno del servidor al procesar la solicitud",
+                            content = @Content(mediaType = "application/json")
+                    )
+            }
     )
 
     @GetMapping("/ingresos/{id}/{month}")
@@ -43,6 +67,38 @@ public class IncomeController {
         return  ResponseEntity.status(HttpStatus.OK).body(allAmount);
     }
 
+
+
+    @Operation(
+            summary = "Obtiene el total de ingresos",
+            description = "Este método realiza una petición a la API de ingresos y gastos, " +
+                    "suma los valores de ingresos  de un usuario y devuelve el resultado " +
+                    "como un balance financiero.",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Cálculo exitoso de la suma de ingresos y gastos",
+                            content = @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = GetTransactionResponse.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "Solicitud incorrecta, posiblemente debido a valores de ingresos o gastos nulos",
+                            content = @Content(mediaType = "application/json")
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "No se encontraron datos de ingresos o gastos para el usuario solicitado",
+                            content = @Content(mediaType = "application/json")
+                    ),
+                    @ApiResponse(
+                            responseCode = "500",
+                            description = "Error interno del servidor al procesar la solicitud",
+                            content = @Content(mediaType = "application/json")
+                    )
+            }
+    )
     @GetMapping("/total/{id}/{month}/ingresos")
     public Mono<ResponseEntity<GetTransactionResponse>> totalIncomeExternalApi(@PathVariable Long id, @PathVariable String month){
         return this.incomeService.calculateTotalIncomeApi(id,month)
