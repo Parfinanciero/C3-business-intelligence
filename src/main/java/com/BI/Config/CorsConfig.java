c2e12dda4320e981b6edd653a9a7b4698acf3f06
Alejandro Velasquez <163916088+alejandro-velasquez@users.noreply.github.com>
2025-01-30 22:02:33 -0500

    Dev (#12)
    * add workflow to ci/cd
    
    * Feature/bi 39 expenses by category (#10)
    
    * feat:implementing prospective methods
    
    * feat:add changes about external Api
    
    * feat:creating goals methods
    
    * fix: update goal method
    
    * feat: add metrical method income vs expenses
    
    * feat: update metrical methods income vs expenses
    
    * loggs
    
    * feat: update configuration cors
    
    * feat: update swagger documentation
    
    * feat: add metric expenses by category
    
    * feat: update expensesByCategory
    
    * feat: update transations method
    
    * update method
    
    * update transactional method
    
    * udapte: changes comments code
    
    * updte code comments
    
    * update: changes about prospective method
    
    * udapte: change name dto propertie totalExpenses to totalCash
    
    * update: changes logs configuration
    
    ---------
    
    Co-authored-by: Nar Mena <narciris.tech@gmail.com>
    
    * update:cors with new route frontend deploy
    
    * update: deploy.yml inside dev
    
    ---------
    
    Co-authored-by: Jcgmtxt <juliancamilo0210@gmail.com>
    Co-authored-by: Nar Mena <narciris.tech@gmail.com>

--- src/main/java/com/BI/Config/CorsConfig.java
+++ src/main/java/com/BI/Config/CorsConfig.java
@@ -10,7 +10,10 @@ public class CorsConfig implements WebMvcConfigurer {
 // configuration cors for frontend
     public void addCorsMappings (CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:5174")
+                .allowedOrigins(
+                        "http://localhost:5174",
+                        "http://23.88.104.53:8045"
+                )
                 .allowedMethods("GET","POST","PUT","DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
